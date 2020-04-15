134eae6fdb9dd61a6a4511f917540389880221f7
rafetefe <rafet.gazanfer27@erzurum.edu.tr>
2020-04-15 06:39:08 +0300

    On branch master

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+      <b-card>
+        <b-button variant="primary" @click="swap()">Json Değiş</b-button>
+        <b-card>
+        <b-card-text v-for="nesne in contentJson" :key="nesne">{{nesne}}</b-card-text>
+        </b-card>
+      </b-card>
+  </div>
+</template>
+
+<script>
+import unixfsJson from './json/unixfs.json'
+import youtubeJson from './json/youtube.json'
+
+export default{
+    data(){
+      return{
+        contentJson : unixfsJson,
+        altJson : youtubeJson,
+        tmp : ""
+      }
+    },
+    methods:{
+      swap(){
+        this.tmp = this.contentJson,
+        this.contentJson = this.altJson,
+        this.altJson = this.tmp
+      }
+    }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
